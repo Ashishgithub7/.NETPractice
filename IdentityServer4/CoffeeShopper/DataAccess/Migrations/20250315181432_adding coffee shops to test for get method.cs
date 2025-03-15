add3e8a6323a88e38a77a7f267d6954c58ab9c72
Ashish Bhandari <ashish7bhandari@gmail.com>
2025-03-16 00:35:03 +0545

    Identity server project completed upto coffeeShop get method controller and created Server project with packages with API and DataAccess projects

---
+++ IdentityServer4/CoffeeShopper/DataAccess/Migrations/20250315181432_adding coffee shops to test for get method.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingcoffeeshopstotestforgetmethod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '6am-10pm', '1234 Main St')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '6am-10pm', 'Belbas')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '5am-12am', 'Butwal')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '24/7', 'Kathmandu')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '5am-12am', 'Pokhara')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '9am-9pm', 'California')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '9am-9pm', 'Miami')");
+            migrationBuilder.Sql($"INSERT INTO CoffeeShops (Name, OpeningHours, Address) VALUES ('Starbucks', '9am-9pm', 'Alaska')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
